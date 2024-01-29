1642a5ca6c4822135ecef835d8bd32287063f8ae
Tony FLORES VALLADARES <r.flores@unitec.edu>
2024-01-28 19:46:36 -0600

    Agregar archivos de proyecto.

---
+++ Tarea1/SuperHeroe.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+
+namespace Tarea1
+{
+    public class SuperHeroe
+    {
+        // Atributos de la clase SuperHeroe
+        public string Nombre { get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string Ciudad { get; set; }
+        public bool PuedeVolar { get; set; }
+        public SuperPoder SuperPoder { get; set; }
+
+        // Constructor de la clase SuperHeroe
+        public SuperHeroe(string nombre, string identidadSecreta, string ciudad, bool puedeVolar, SuperPoder superPoder)
+        {
+            Nombre = nombre;
+            IdentidadSecreta = identidadSecreta;
+            Ciudad = ciudad;
+            PuedeVolar = puedeVolar;
+            SuperPoder = superPoder;
+        }
+
+        // Método para mostrar la información del superhéroe
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+            Console.WriteLine($"Puede Volar: {PuedeVolar}");
+            Console.WriteLine($"SuperPoder: {SuperPoder.Nombre}");
+            Console.WriteLine($"Descripción del SuperPoder: {SuperPoder.Descripcion}");
+            Console.WriteLine($"Nivel del SuperPoder: {SuperPoder.Nivel}");
+            Console.WriteLine();
+        }
+    }
+}
